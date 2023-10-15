33c17a81e9f588cf72eee5b395fce4ca2ca7eda1
Eriuden <antoinecarrette@hotmail.fr>
2023-10-15 20:20:35 +0200

    header

--- client/src/redux/reducers/indexReducer.js
+++ client/src/redux/reducers/indexReducer.js
@@ -1,6 +1,15 @@
 import { combineReducers } from "redux";
+import userReducer from "./user.reducer";
+import usersReducer from "./allUsers.reducer";
+import errorReducer from "./error.reducer";
+import gunplaReducer from "./gunpla.reducer";
+import allGunplasReducer from "./allGunplas.reducer";
 
 export const reducers = combineReducers({
-
+userReducer,
+usersReducer,
+gunplaReducer,
+allGunplasReducer,
+errorReducer,
 })
 
