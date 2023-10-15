abfcf30b46292209b4e4970c9801593cbc19abc1
Eriuden <antoinecarrette@hotmail.fr>
2023-10-15 19:58:01 +0200

    search

--- client/src/components/Searcher.js
+++ client/src/components/Searcher.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { getAllGunpla } from '../redux/actions/gunpla.action'
+import { useSelector, UseSelector } from 'react-redux/es/hooks/useSelector'
+import { GunplaCard } from './GunplaCard'
 
 export const Searcher = () => {
+
+  const [search, setSearch] = useState("")
+  const gunplas = useSelector((state) => state.gunplaReducer)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getAllGunpla)
+  }, [search])
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value)
+
+    const result = gunplas.filter((gunpla)=> gunpla.name.toLowerCase()
+    .includes(search.toLowerCase()) || gunpla.grade.toLowerCase()
+    .includes(search.toLowerCase))
+
+    setSearch(result)
+  }
+
+
   return (
-    <div>Searcher</div>
+    <div>
+      <form action='' className='my-4'>
+        <input type="text"
+        placeholder='Entrez un nom' id="search-input"
+        onChange={handleSearchChange} value={search}/>
+      </form>
+
+      <div>
+        {search ? (
+          gunplas.map((gunpla)=> {
+            if (gunpla.name.includes(search)){
+              <GunplaCard gunplaProps={gunpla} key={gunpla._id}/>
+            }
+          })
+        ): ""}
+      </div>
+    </div>
   )
 }
