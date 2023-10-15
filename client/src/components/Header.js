7db1714025635f26571d7a663c98794bfac95bdb
Eriuden <antoinecarrette@hotmail.fr>
2023-10-15 19:48:03 +0200

    logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
+import { UseSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { UidContext } from './AppContext'
+import {Squash as Hamburger} from "hamburger-react"
+
+
 
 export const Header = () => {
   return (
---
+++ client/src/components/Logout.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import axios from 'axios'
+import cookie from "js-cookie"
+
+export const Logout = () => {
+  const removeCookie = (key) => {
+    if (window !== "undefined") {
+        cookie.remove(key, {expires:1})
+    }
+  }
+
+  const unlog = async () => {
+    await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}api/user/logout`,
+        withCredentials:true,
+    })
+    .then(()=> removeCookie("jwt"))
+    .catch((err)=> window.alert(err))
+
+    window.location="/"
+  }
+
+  return (
+    <div>
+        <li onClick={unlog}>
+            <h3>Se déconnecter</h3>
+        </li>
+    </div>
+  )
+}
