0bcb91e2aafa5be5e634797bc275efbfa38390a5
Eriuden <antoinecarrette@hotmail.fr>
2023-10-17 14:58:20 +0200

    Connexion

--- client/src/pages/Connexion.js
+++ client/src/pages/Connexion.js
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import {Link} from "react-router-dom"
 
 export const Connexion = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = (e)=> {
+    e.preventDefault()
+    const emailError = document.querySelector(".email.error")
+    const passwordError = document.querySelector(".password.error")
+
+    axios({
+      method:"post",
+      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      withCredentials:true,
+      data: {
+        email,
+        password,
+      },
+    })
+    .then((res)=> {
+      if(res.data.errors) {
+        emailError.innerHTML = res.data.errors.email
+        passwordError.innerHTML = res.data.errors.password
+      } else {
+        window.location="/"
+      }
+    })
+    .catch((err)=> {
+      window.alert(err)
+    })
+  }
+
   return (
-    <div>Connexion</div>
+    <div>
+      <form action='' onSubmit={handleLogin} className="bg-slate-50 border-l-4 
+      rounded-md border-2 border-black
+      mx-6 my-4 sm:mx-40 md:text-base lg:mx-96 text-base">
+        <label htmlFor='email'>Email</label>
+        <br/>
+
+        <input className='border-2 border-black' 
+          type="text"
+          name='email'
+          id='email'
+          onChange={(e)=> setEmail (e.target.value)}
+          value={email}
+        />
+
+        <div className='email error'></div>
+        <br/>
+
+        <input className='border-2 border-black'
+          type="password"
+          name='password'
+          id='password'
+          onChange={(e)=> setPassword (e.target.value)}
+          value={password}
+        />
+
+        <div className="password error"></div>
+        <br/>
+
+        <input className='border-2 border-black my-2 px-2' 
+          type="submit"
+          value="connexion"
+        />
+
+        <Link to={"/reset-password"} className="border-2 border-black">
+          Mot de passe oublié ?
+        </Link>
+      </form>
+    </div>
   )
 }
