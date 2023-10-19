2abc3b558c957f92aebb19cc2fe87f93c5de6dd1
Eriuden <antoinecarrette@hotmail.fr>
2023-10-19 10:20:55 +0200

    card model

--- client/src/components/GunplaCard.js
+++ client/src/components/GunplaCard.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import {useSelector} from "react-redux"
+import { isEmpty } from '../utils'
 
-export const GunplaCard = () => {
+export const GunplaCard = ({gunplaProps}) => {
+  const gunplas = useSelector((state)=> state.allGunplasReducer)
+
+  
   return (
-    <div>GunplaCard</div>
+    <div key= {gunplaProps._id}>
+      <>
+        <div>
+          <img src={!isEmpty(gunplas[0] && 
+          gunplas.map((gunpla) => {
+            if (gunpla._id) return gunpla.picture
+          }))}
+          />
+
+          <h3>{!isEmpty(gunplas[0] && 
+          gunplas.map((gunpla) => {
+            if (gunpla._id) return gunpla.name
+          }))}
+          </h3>
+
+          <h3>{!isEmpty(gunplas[0] && 
+          gunplas.map((gunpla) => {
+            if (gunpla._id) return gunpla.grade
+          }))}
+          </h3>
+
+          <h3>{!isEmpty(gunplas[0] && 
+          gunplas.map((gunpla) => {
+            if (gunpla._id) return gunpla.price
+          }))}
+          </h3>
+        </div>
+      </>
+    </div>
   )
 }
